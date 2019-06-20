5d02ef039939a3d35a6729c47f93517ed9dd3bc2
cangyu1993 <1765244720@qq.com>
2019-06-20 17:05:53 +0800

    引入draggab拖动

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,16 +3,28 @@
         <touch-ripple>
             <el-button type="primary">点击测试</el-button>
         </touch-ripple>
+        <div class="groups">
+            <draggable v-model="myArray" group="people" @start="drag=true" @end="drag=false">
+                <div class="groups-item" v-for="element in myArray" :key="element.id">{{element.name}}</div>
+            </draggable>
+        </div>
     </div>
 </template>
 
 <script>
     import api from '@/axios/api'
-
+    import draggable from 'vuedraggable'
     export default {
+        components: {  draggable },
         name: 'HelloWorld',
         props: {
             msg: String
+        },
+        data(){
+            return{
+                myArray:[{id:0,name:'tom'},{id:1,name:'jim'},{id:2,name:'sair'}],
+                drag:false
+            }
         },
         methods: {
             getData() {
@@ -48,5 +60,12 @@
 
     a {
         color: #42b983;
+    }
+    .groups>div{
+        width: 100vw;
+        .groups-item{
+            width: 200px;
+            height: 200px;
+        }
     }
 </style>
---
+++ src/publicJs/drag.js
@@ -0,0 +1,32 @@
+const drag = {
+    // 初始化指令
+    bind: function (el) {
+        let odiv = el;   //获取当前元素
+        odiv.onmousedown = (e) => {
+            //算出鼠标相对元素的位置
+            let disX = e.clientX - odiv.offsetLeft;
+            let disY = e.clientY - odiv.offsetTop;
+
+            document.onmousemove = (e) => {
+                //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+                let left = e.clientX - disX;
+                let top = e.clientY - disY;
+
+                //绑定元素位置到positionX和positionY上面
+                // this.positionX = top;
+                // this.positionY = left;
+
+                //移动当前元素
+                odiv.style.left = left + 'px';
+                odiv.style.top = top + 'px';
+            };
+            document.onmouseup = (e) => {
+                document.onmousemove = null;
+                document.onmouseup = null;
+            };
+        };
+    }
+}
+
+
+export default drag
